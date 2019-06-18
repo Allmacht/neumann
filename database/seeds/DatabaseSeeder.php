ecab174f721618a5cc12ff3d2a18e648acc0e839
Allmacht <ulises.jacob.cr@gmail.com>
2019-06-18 18:20:26 -0500

    Se Añade función truncateTables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->truncateTables([
+            'roles'
+        ]);
+
+        $this->call(RolesSeeder::class);
+        
+    }
+    protected function truncateTables(array $tables)
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+
+        foreach ($tables as $table) :
+            DB::table($table)->truncate();
+        endforeach;
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 }
