82d2808e041e4cede4f155a0b0d542c1e1b4921a
Allmacht <ulises.jacob.cr@gmail.com>
2019-06-18 18:16:37 -0500

    Creación del RolesSeeder

---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $SAdministrador = Role::create([
+            'name' => 'SAdministrador',
+        ]);
+
+        $Administrador = Role::create([
+            'name' => 'Administrador',
+        ]);
+        
+        $Docente = Role::create([
+            'name' => 'Docente',
+        ]);
+
+        $Coordinador = Role::create([
+            'name' => 'Coordinador',
+        ]);
+
+        $Bibliotecario = Role::create([
+            'name' => 'Bibliotecario',
+        ]);
+
+        $Estudiante = Role::create([
+            'name' => 'Estudiante',
+        ]);
+    }
+}
