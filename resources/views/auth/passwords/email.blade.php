67f9b2bcf92a9aefa7bf620af2d16cdb85fec828
Allmacht <ulises.jacob.cr@gmail.com>
2019-06-19 11:38:46 -0500

    Correción login.css

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,17 @@
 @extends('layouts.app')
-
+@section('title','Restablecer contreña - Neumann')
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/Auth/email.css') }}">
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-8 col-lg-8 col-md-10 col-sm-12 div-1 offset-xl-2 offset-lg-2 py-3 shadow">
+                <h1 class="text-truncate text-header text-center">
+                    {{ __('Restablecer su contraseña') }}
+                </h1>
+                <p class="text-center py-3 padding-text">{{ __('Ingrese su correo electónico asociado a su cuenta, recibirá un email con un enlace para restablecer su contraseña') }}</p>
             </div>
         </div>
     </div>
-</div>
 @endsection
