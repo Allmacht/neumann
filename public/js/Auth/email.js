c4c50a3e0dfecb08b95aff3f57be4ed7a5850768
Allmacht <ulises.jacob.cr@gmail.com>
2019-06-21 13:03:17 -0500

    Correción de Auth/email.js

--- public/js/Auth/email.js
+++ public/js/Auth/email.js
@@ -1,8 +1,15 @@
-$(document).ready(function(){
-
-    $('#submit-email').click(function(){
-         var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        if ($('#email').val().trim().length > 0 && regex.test($('#email').val().trim())) {
+$(document).ready(function() {
+    var email = false;
+    $('#email').keyup(function() {
+      var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      if ($('#email').val().trim().length > 0 && regex.test($('#email').val().trim())) {
+        email = true;
+      }else{
+        email = false;
+      }
+    });
+    $('#submit-email').click(function() {
+        if (email == true) {
             $(this).empty().addClass('btn-login').append('<i class="fas fa-spinner fa-pulse fa-2x"></i>');
             $('#cancel-link').attr('hidden', true);
         }
