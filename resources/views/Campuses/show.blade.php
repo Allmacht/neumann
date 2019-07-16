399d659932f675c63480b4d37826dfee30a00b9a
Allmacht <ulises.jacob.cr@gmail.com>
2019-07-16 11:41:38 -0500

    Correción al Campiuses.show

--- resources/views/Campuses/show.blade.php
+++ resources/views/Campuses/show.blade.php
@@ -7,137 +7,154 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 mt-5 px-4 mb-3">
-                <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 float-left py-3 text-center">
-                    <div class="alert alert-primary shadow">
-                        <h6 class="text-muted"><strong>{{__('Código único : ')}}</strong>{{{$campus->code}}}</h6>
-                        <h6 class="text-muted"><strong>{{__('CCT : ')}}</strong>{{{$campus->cct}}}</h6>
-                        <hr>
-                        <div class="my-4">
-                            <h3>
-                                {{$campus->name}}
-                            </h3>
-                        </div>
+            <div class="col-12 mt-4 px-4 mb-3">
+                <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 float-left text-center alert alert-primary shadow show-left">
+                    <h6 class="text-muted"><strong>{{__('Código único : ')}}</strong>{{{$campus->code}}}</h6>
+                    <h6 class="text-muted"><strong>{{__('CCT : ')}}</strong>{{{$campus->cct}}}</h6>
+                    <hr>
+                    <div class="my-4">
+                        <h3>
+                            {{$campus->name}}
+                        </h3>
                     </div>
-                    <div class="bg-white shadow rounded py-3">
-                        <h6>{{__('Acciones')}}</h6>
-                        <hr>
-                        <div class="form-row">
-                            <a href="{{route('campuses.edit',['id'=>$campus->id])}}" class="btn btn-success mx-auto">
-                                <i class="fas fa-edit"></i>
-                                {{__('Editar')}}
-                            </a>
-                            <div class="dropdown mx-auto">
-                                <button class="btn btn-danger dropdown-toggle" type="button" data-toggle="dropdown">
-                                    <i class="fas fa-file-pdf"></i>
-                                    {{__('PDF')}}
-                                </button>
-                                <div class="dropdown-menu">
-                                    <a href="{{route('campuses.show.pdf',['id'=>$campus->id])}}" class="dropdown-item btn-pdf">
-                                        <div class="float-left w-25 text-center">
-                                            <i class="fas fa-file-download text-danger"></i>
-                                        </div>
-                                        <div class="float-right w-75 text-center">
-                                            {{__(' Descargar')}}
-                                        </div>
-                                    </a>
-                                    <button data-toggle="modal" data-target="#sendpdf" class="dropdown-item btn-pdf">
-                                        <div class="float-left w-25 text-center">
-                                            <i class="fas fa-share-square text-danger"></i>
-                                        </div>
-                                        <div class="float-right w-75 text-center">
-                                            {{__(' Enviar')}}
-                                        </div>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <a href="{{route('campuses.edit', ['id'=>$campus->id])}}" class="btn btn-success btn-sm rounded-pill">
+                        <i class="fas fa-edit"></i>
+                        {{__('Editar')}}
+                    </a>
                 </div>
-                <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 float-right py-2 shadow table-responsive bg-white">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>{{__('Clave')}}</th>
-                                <th>{{__('Valor')}}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{{__('Código único')}}</td>
-                                <td>{{$campus->code}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{__('CCT')}}</td>
-                                <td>{{$campus->cct}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{__('Nombre')}}</td>
-                                <td>{{$campus->name}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{__('Estado')}}</td>
-                                <td>{{$campus->state}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{__('Municipio')}}</td>
-                                <td>{{$campus->municipality}}</td>
-                            </tr>
-                            @if ($campus->colony != null)
+                <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 float-right py-2 table-responsive">
+                    <!--Tabla de información-->
+                    <div class="alert alert-success text-center mb-0 shadow form-row" style="z-index: 0">
+                        <div class="col-10 text-left">
+                            <h4 class="alert-heading mb-0">{{__('Información General')}}</h4>
+                        </div>
+                        <div class="col-2 text-right">
+                            <i id="collapse-icon" class="fas fa-arrow-circle-up fa-lg" style="cursor: pointer" data-toggle="collapse" data-target="#table-info"></i>
+                        </div>
+                    </div>
+                    <div class="collapse show mb-3" id="table-info" style="z-index: -1">
+                        <table class="table table-hover bg-white shadow">
+                            <thead>
                                 <tr>
-                                    <td>{{__('Colonia/Fracc.')}}</td>
-                                    <td>{{$campus->colony}}</td>
+                                    <th class="align-middle pt-3">{{__('Clave')}}</th>
+                                    <th class="align-middle pt-3">{{__('Valor')}}</th>
                                 </tr>
-                            @endif
-                            <tr>
-                                <td>{{__('Calle')}}</td>
-                                <td>{{$campus->street}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{__('Código postal')}}</td>
-                                <td>{{$campus->zipcode}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{__('Número exterior')}}</td>
-                                <td>{{$campus->external_number}}</td>
-                            </tr>
-                            @if ($campus->internal_number != null) 
+                            </thead>
+                            <tbody>
                                 <tr>
-                                    <td>{{__('Número interior')}}</td>
-                                    <td>{{$campus->internal_number}}</td>
+                                    <td class="align-middle">
+                                        {{__('Acciones')}}
+                                    </td>
+                                    <td class="align-middle">
+                                        <a href="{{route('campuses.show.pdf',['id'=>$campus->id])}}" class="btn btn-danger btn-sm rounded-pill">
+                                            <i class="fas fa-file-pdf"></i>
+                                            {{__('Descargar PDF')}}
+                                        </a>
+                                        <button class="btn btn-info btn-sm rounded-pill" data-toggle="modal" data-target="#sendpdf">
+                                            <i class="fas fa-paper-plane"></i>
+                                            {{__('Enviar PDF')}}
+                                        </button>
+                                    </td>
                                 </tr>
-                            @endif
-                            <tr>
-                                <td class="align-middle">{{__('Administrador')}}</td>
-                                <td>
-                                    <a href="" class="btn btn-outline-primary" data-toggle="tooltip" data-placement="right" title="Ver perfil">
-                                        <i class="fas fa-user-circle"></i>
-                                        {{$campus->user->names." ".$campus->user->paternal_surname}}
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>{{__('Turno')}}</td>
-                                <td>{{$campus->shift}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{__('Nivel escolar')}}</td>
-                                <td>{{$campus->level}}</td>
-                            </tr>
-                            <tr class="@if($campus->status) table-success @else table-danger @endif">
-                                <td>{{__('Estado')}}</td>
-                                @if ($campus->status)
-                                    <td>{{__('Activo')}}</td>    
-                                @else
-                                    <td>{{__('Desactivado')}}</td>  
+                                <tr>
+                                    <td>{{__('Código único')}}</td>
+                                    <td>{{$campus->code}}</td>
+                                </tr>
+                                <tr>
+                                    <td>{{__('CCT')}}</td>
+                                    <td>{{$campus->cct}}</td>
+                                </tr>
+                                <tr>
+                                    <td>{{__('Nombre')}}</td>
+                                    <td>{{$campus->name}}</td>
+                                </tr>
+                                <tr>
+                                    <td>{{__('Estado')}}</td>
+                                    <td>{{$campus->state}}</td>
+                                </tr>
+                                <tr>
+                                    <td>{{__('Municipio')}}</td>
+                                    <td>{{$campus->municipality}}</td>
+                                </tr>
+                                @if ($campus->colony != null)
+                                    <tr>
+                                        <td>{{__('Colonia/Fracc.')}}</td>
+                                        <td>{{$campus->colony}}</td>
+                                    </tr>
                                 @endif
-                            </tr>
-                            <tr>
-                                <td>{{__('Fecha de registro')}}</td>
-                                <td>{{$campus->created_at}}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                                <tr class="">
+                                    <td>{{__('Calle')}}</td>
+                                    <td>{{$campus->street}}</td>
+                                </tr>
+                                <tr>
+                                    <td>{{__('Código postal')}}</td>
+                                    <td>{{$campus->zipcode}}</td>
+                                </tr>
+                                <tr>
+                                    <td>{{__('Número exterior')}}</td>
+                                    <td>{{$campus->external_number}}</td>
+                                </tr>
+                                @if ($campus->internal_number != null) 
+                                    <tr>
+                                        <td>{{__('Número interior')}}</td>
+                                        <td>{{$campus->internal_number}}</td>
+                                    </tr>
+                                @endif
+                                <tr>
+                                    <td class="align-middle">{{__('Administrador')}}</td>
+                                    <td>
+                                        <a href="" class="btn btn-outline-primary" data-toggle="tooltip" data-placement="right" title="Ver perfil">
+                                            <i class="fas fa-user-circle"></i>
+                                            {{$campus->user->names." ".$campus->user->paternal_surname}}
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>{{__('Turno')}}</td>
+                                    <td>{{$campus->shift}}</td>
+                                </tr>
+                                <tr>
+                                    <td>{{__('Nivel escolar')}}</td>
+                                    <td>{{$campus->level}}</td>
+                                </tr>
+                                <tr>
+                                    <td>{{__('Estado')}}</td>
+                                    @if ($campus->status)
+                                        <td>{{__('Activo')}}</td>    
+                                    @else
+                                        <td>{{__('Desactivado')}}</td>  
+                                    @endif
+                                </tr>
+                                <tr>
+                                    <td>{{__('Fecha de registro')}}</td>
+                                    <td>{{$campus->created_at}}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!--fin tabla de información-->
+
+                    <!--tabla de carreras-->
+                    <div class="alert alert-info text-center mb-0 mt-4 shadow form-row" style="z-index: 0">
+                        <div class="col-10 text-left">
+                            <h4 class="alert-heading mb-0">{{__('Carreras disponibles')}}</h4>                            
+                        </div>
+                        <div class="col-2 text-right">
+                            <i id="collapse-icon-degrees" class="fas fa-arrow-circle-up fa-lg" style="cursor: pointer" data-toggle="collapse" data-target="#table-degrees"></i>
+                        </div>
+                    </div>
+                    <div class="collapse show mb-3" id="table-degrees" style="z-index: -1">
+                        <table class="table table-hover bg-white shadow">
+                            <thead>
+                                <tr>
+                                    <th>{{__('ID')}}</th>
+                                    <th>{{__('Nombre')}}</th>
+                                    <th>{{__('Duración')}}</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <!--fin tabla de carreras-->
                 </div>
             </div>
         </div>
