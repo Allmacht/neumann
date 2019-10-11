57c5f2cfd9472f4cf676e96c37db7eef77110f08
isaaccruz94 <crherrera94@hotmail.com>
2019-10-11 17:28:57 -0500

    diseño menu bibliotecario para administrativos

--- resources/views/libraries/index.blade.php
+++ resources/views/libraries/index.blade.php
@@ -2,13 +2,33 @@
 @section('libraries', 'active-nav')
 @section('title','Bibliotecas')
 @section('styles')
-    <link rel="stylesheet" href="{{asset('css/Libraries/index.css')}}">
+    <link rel="stylesheet" href="{{asset('css/Libraries/admin.css')}}">
     @section('content')
    <div class="container-fluid">
        <div class="row">
            <div class="col-12 px-5 py-4">
-               <h1 class="text-truncate text-title text-md-left text-center">{{__('Bibliotecas')}}</h1>
+               <h1>{{__('Bibliotecas')}}</h1>
+               <hr>
            </div>
        </div>
+   </div>
+   <div class="container">
+        <div class="row menu">
+            <div class="col-sm contenido">
+                <a href="">agregar libro</a>
+            </div>
+            <div class="col-sm contenido">
+                <a href="">prestamos</a>
+            </div>
+            <div class="col-sm contenido">
+                <a href="">borrar libro</a>
+            </div>
+            <div class="col-sm contenido">
+                <a href="">preguntas</a>
+            </div>
+            <div class="col-sm contenido">
+                <a href="">blibliotecas</a>
+            </div>
+        </div>
    </div>
     @endsection
